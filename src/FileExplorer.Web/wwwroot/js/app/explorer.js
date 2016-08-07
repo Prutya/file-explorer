6cb4e8efb994b61ccbe7260af9fa6dbf68eec1fb
Anton Prudkohliad <anton.prudkohliad@gmail.com>
2016-08-07 22:54:01 +0300

    Add files count table

--- src/FileExplorer.Web/wwwroot/js/app/explorer.js
+++ src/FileExplorer.Web/wwwroot/js/app/explorer.js
@@ -1,14 +1,11 @@
 var app = angular.module('explorerApp', []);
 
 app.controller('explorerController', function ($scope, $http) {
-    $http.get("/api/explorer")
-        .then(function (response) {
-            $scope.data = response.data;
-        });
     $scope.changeDir = function (path) {
         $http.get("/api/explorer?path=" + path)
-        .then(function (response) {
-            $scope.data = response.data;
-        });
+            .then(function (response) {
+                $scope.data = response.data;
+            });       
     };
+    $scope.changeDir("");
 });
